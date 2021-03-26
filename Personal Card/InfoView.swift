6fa5293295ac1233c4420761e8ab8aab9aa08a56
FrancaTM <francatm@outlook.com>
2021-03-26 12:38:25 -0300

    create info view

---
+++ Personal Card/InfoView.swift
@@ -0,0 +1,30 @@
+//
+//  InfoView.swift
+//  Personal Card
+//
+//  Created by Tulio Marcos Franca on 26/03/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+        
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .aspectRatio(contentMode: .fill)
+                    .foregroundColor(.green)
+                Text(text).bold()
+            }).padding(.all)
+    }
+}
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Teste", imageName: "phone.fill").previewLayout(.sizeThatFits)
+    }
+}
